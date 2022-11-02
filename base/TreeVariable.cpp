36e13e39274857aa68966c78b7f66b0d750bf497
Ron Fox <fox@nscl.msu.edu>
2022-11-02 15:09:38 +0000

    Define new CTreeVariable class - good header compilation

---
+++ base/TreeVariable.cpp
@@ -0,0 +1,27 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     FRIB
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  TreeVariable.cpp
+ *  @brief: Implementation of the Tree Variable class
+ */
+#include "TreeVariable.h"
+
+namespace frib {
+    namespace analysis {
+        
+    }
+}
