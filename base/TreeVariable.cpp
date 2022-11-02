6c9d761f10eb5f525ba12b52c250cb7572b62293
Ron Fox <fox@nscl.msu.edu>
2022-11-02 15:16:11 +0000

    Implemnted  constructors for CTreeVariable::Definition struct

--- base/TreeVariable.cpp
+++ base/TreeVariable.cpp
@@ -23,5 +23,37 @@
 namespace frib {
     namespace analysis {
         
+        // Constructors for CTreeVariable::Definition:
+        
+        /**
+         * Default construtor value -> 0.0, units ""
+         */
+        CTreeVariable::Definition::_Definition() :
+            s_value(0.0), s_units(""), s_definitionChanged(false), s_valueChanged(false)
+        {}
+        /**
+         * constructor:
+         *   @param value - initial value of the variable:
+         */
+        CTreeVariable::Definition::_Definition(double value) :
+            s_value(value), s_units(""), s_definitionChanged(false), s_valueChanged(false)
+        {}
+        /**
+         * constructor
+         *  @param value -initial value
+         *  @param pUnits - unists of measure.
+         */
+        CTreeVariable::Definition::_Definition(double value, const char* pUnits) :
+            s_value(value), s_units(pUnits), s_definitionChanged(false), s_valueChanged(false)
+        {}
+        /**
+         * copy constructor
+         */
+        CTreeVariable::Definition::_Definition(const _Definition & rhs) :
+            s_value(rhs.s_value), s_units(rhs.s_units),
+            s_definitionChanged(rhs.s_definitionChanged),
+            s_valueChanged(rhs.s_valueChanged)
+        {}
+         
     }
 }
