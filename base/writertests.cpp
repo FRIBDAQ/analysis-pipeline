44703594c13fd4c7e1b2bf4dcd15c7d0a536ba09
Ron Fox <fox@nscl.msu.edu>
2022-11-10 12:42:47 -0500

    Get setup/teardown on DataWriter done

--- base/writertests.cpp
+++ base/writertests.cpp
@@ -21,9 +21,22 @@
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/Asserter.h>
 #include "Asserts.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <new>
+#include <stdexcept>
+#include <string>
+#include <string.h>
 
 #include "DataWriter.h"
 using namespace frib::analysis;
+
+static const char* templateFilename="testXXXXXX.dat";
+
 
 class writertest : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(writertest);
@@ -31,13 +44,29 @@ class writertest : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE_END();
     
 private:
-
+    int m_fd;
+    std::string m_filename;
 public:
     void setUp() {
-        
+        // Make a temporary file in which the data can be written.
+
+        char ftemplate[100];
+        strncpy(ftemplate, templateFilename, sizeof(ftemplate));
+        m_fd = mkstemps(ftemplate, 4);     // 4 '.dat'
+        if (m_fd < 0) {
+            std::string failmsg = "Failed to make tempfile: ";
+            failmsg += strerror(errno);
+            throw std::runtime_error(failmsg);
+        }
+        m_filename = ftemplate;
+
     }
     void tearDown() {
-        
+         // CLose the tempfile and remove it...b/c/ temp files aren't atually temp
+
+        close(m_fd);      // Might have been closed in test so don't check status
+        unlink(m_filename.c_str());
+
     }
 protected:
     void test_1();
