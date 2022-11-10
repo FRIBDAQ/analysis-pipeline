921314eb9866aa0e5248f0b9fec76c871a814215
Ron Fox <fox@nscl.msu.edu>
2022-11-10 08:04:31 -0500

    Test construction failure on malloc fail

--- base/readertests.cpp
+++ base/readertests.cpp
@@ -43,6 +43,8 @@ static const char* templateFilename="testXXXXXX.dat";
 class readertest : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(readertest);
     CPPUNIT_TEST(construct_1);
+    CPPUNIT_TEST(construct_2);
+    CPPUNIT_TEST(construct_3);
     CPPUNIT_TEST_SUITE_END();
     
 private:
@@ -66,13 +68,47 @@ public:
     void tearDown() {
         // CLose the tempfile and remove it...b/c/ temp files aren't atually temp.
         
-        close(m_fd);
+        close(m_fd);      // Might have been closed in test so don't check status
         unlink(m_filename.c_str());
         
     }
 protected:
     void construct_1();
+    void construct_2();
+    void construct_3();
+    
+    // Utilities:
+    
+    void writeCountPattern(std::uint32_t nBytes, std::uint8_t start, std::uint8_t incr);
 };
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+// Utility methods:
+
+/**
+ * writeCountPattern
+ *   Write a ring item with a payload that consists of a byte sized counting
+ *   pattern:
+ *
+ * @param nBytes - total number of bytes -inluding self.
+ * @param start  - Initial value of couting pattern.
+ * @param incr   - increment between items.
+ */
+void
+readertest::writeCountPattern(
+    std::uint32_t nBytes, std::uint8_t start, std::uint8_t incr
+) {
+    // For now assume the writes will work:
+    
+    
+    write(m_fd, &nBytes, sizeof(uint32_t));
+    nBytes -= sizeof(std::uint32_t);              // Payload size.
+    for (int i=0; i < nBytes; i++) {
+        write(m_fd, &start, sizeof(start));
+        start += incr;
+    }
+}
 
 CPPUNIT_TEST_SUITE_REGISTRATION(readertest);
 
@@ -86,4 +122,25 @@ void readertest::construct_1()
     ASSERT(d.m_eof);
     ASSERT(d.m_fReleased);
        
+}
+// Construct on empty file by fd:
+
+void readertest::construct_2()
+{
+    CDataReader d(m_fd, 100);
+    EQ(m_fd, d.m_nFd);
+     EQ(size_t(0), d.m_nBytes);
+    ASSERT(d.m_pBuffer);
+    EQ(size_t(100), d.m_nBufferSize);
+    ASSERT(d.m_eof);
+    ASSERT(d.m_fReleased);
+    
+}
+// Very likely a petabyte buffer will fail:
+
+void readertest::construct_3() {
+    CPPUNIT_ASSERT_THROW(
+        CDataReader d(m_fd, size_t(1024UL*1024UL*1024UL*1024UL)),
+        std::bad_alloc
+    );
 }
