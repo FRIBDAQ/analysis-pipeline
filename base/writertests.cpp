0f40061a2db5ea98a7a7034cd3e22a25c2734759
Ron Fox <fox@nscl.msu.edu>
2022-11-14 09:29:26 -0500

    Gested writeItem method

--- base/writertests.cpp
+++ base/writertests.cpp
@@ -57,6 +57,10 @@ class writertest : public CppUnit::TestFixture {
     CPPUNIT_TEST(construct_5);
     CPPUNIT_TEST(construct_6);
     CPPUNIT_TEST(construct_7);
+    
+    CPPUNIT_TEST(write_1);
+    CPPUNIT_TEST(write_2);
+    CPPUNIT_TEST(write_3);
     CPPUNIT_TEST_SUITE_END();
     
 private:
@@ -93,7 +97,74 @@ protected:
     void construct_5();
     void construct_6();
     void construct_7();
+    
+    void write_1();
+    void write_2();
+    void write_3();
+private:
+        void* makeCountingRingItem(
+            void* pBuffer,
+            std::uint32_t totalSize, std::uint8_t first, std::uint8_t step
+        );
+        const void* skipItems(const void* pBuffer, size_t nItems=1);
 };
+
+/**
+ * skipItems
+ * 
+ *   Skip buffered ring item(s).
+ * @param pBuffer - Buffer containing a sequence of ring items.
+ * @param nItems - number of items to skip.
+ * @note the caller is responsible for determining there are at least nItems
+ * @return void* pointer to the ring item after skippgin nItesm in the buffer.
+ *
+ */
+const void*
+writertest::skipItems(const void* pBuffer, size_t nItems) {
+    for (int i =0; i < nItems; i++) {
+        union {
+            const std::uint8_t* p8;
+            const RingItemHeader* ph;
+        } p;
+        p.p8 = reinterpret_cast<const std::uint8_t*>(pBuffer);
+        p.p8 += p.ph->s_size;
+        pBuffer = p.p8;
+    }
+    return pBuffer;
+}
+/**
+ * makeCountingRingItem
+ *    Create a counting ring item.
+ *  @param pBuffer - user buffer must bet at least totalSize bytes of storage.
+ *  @param totalSize - Total number of bytes in the ring item to create.
+ *  @param first    - Value of first byte of body.
+ *  @param step     - Next item step added to prior.
+ *  @return void*    - pBuffer
+ *
+ *  @note the item type will be TEST_DATA, of course.
+ */
+void*
+writertest::makeCountingRingItem(
+        void* pBuffer,
+        std::uint32_t totalSize, std::uint8_t first, std::uint8_t step
+) {
+    ASSERT(totalSize >= sizeof(RingItemHeader));   // At least an empty item.
+    pRingItemHeader pHeader = reinterpret_cast<pRingItemHeader>(pBuffer);
+    pHeader->s_size = totalSize;
+    pHeader->s_type = TEST_DATA;
+    pHeader->s_unused = sizeof(std::uint32_t);
+    
+    pHeader++;
+    std::uint8_t* p = reinterpret_cast<std::uint8_t*>(pHeader);
+    totalSize -= sizeof(RingItemHeader);                // Remaining bytes:
+    
+    for (int i =0; i < totalSize; i++) {
+        *p++ = first;
+        first += step;
+    }
+    return pBuffer;
+}
+
 
 CPPUNIT_TEST_SUITE_REGISTRATION(writertest);
 
@@ -295,4 +366,87 @@ void writertest::construct_7()
         EQ(0, strcmp(a[i].getName().c_str(), p.pv->s_variableName));
         p.p8 += sizeof(Variable) + strlen(p.pv->s_variableName) +1;
     }
+}
+// Write an empty ring item:
+
+void writertest::write_1()
+{
+    RingItemHeader item;
+    void* pItem = makeCountingRingItem(&item, sizeof(item), 0, 0);
+    {
+        CDataWriter w(m_filename.c_str());
+        w.writeItem(pItem);
+    }
+    // The file should have three items - empty parameter defs, empty var values
+    // and the empty ring item in item.
+    
+    CDataReader reader(m_fd, 8192*10);
+    auto r = reader.getBlock(8192*10);
+    EQ(size_t(3), r.s_nItems);
+    
+    const void* pReadItem = skipItems(r.s_pData, 2); // s.b. item.
+    EQ(0, memcmp(pItem, pReadItem, sizeof(RingItemHeader)));
+    
+}
+// Write a ring item with some contents:
+
+void writertest::write_2()
+{
+    std::uint32_t item[8192];
+    void* pItem = makeCountingRingItem(item, 100, 1, 1);
+    {
+        CDataWriter w(m_filename.c_str());
+        w.writeItem(pItem);
+    }
+    // The file should have three items - empty parameter defs, empty var values
+    // and the empty ring item in item.
+    
+    CDataReader reader(m_fd, 8192*10);
+    auto r = reader.getBlock(8192*10);
+    EQ(size_t(3), r.s_nItems);
+    
+    const void* pReadItem = skipItems(r.s_pData, 2); // s.b. item.
+    
+    // Ensure the size is right:
+    
+    const RingItemHeader* pHeader = reinterpret_cast<const RingItemHeader*>(pReadItem);
+    EQ(size_t(100), size_t(pHeader->s_size));
+    EQ(TEST_DATA, pHeader->s_type);
+    EQ(sizeof(std::uint32_t), size_t(pHeader->s_unused));
+    
+    EQ(0, memcmp(item, pReadItem, 100));
+    
+}
+// write a couple non-empty ring items
+void writertest::write_3() {
+    std::uint32_t item1[200];
+    std::uint32_t item2[100];
+    void* pItem1 = makeCountingRingItem(item1, 200, 1, 1);
+    void* pItem2 = makeCountingRingItem(item2, 100, 1, 2);
+    
+    {
+        CDataWriter w(m_filename.c_str());
+        w.writeItem(item1);
+        w.writeItem(item2);
+    }
+    // The file should have three items - empty parameter defs, empty var values
+    // and the empty ring item in item.
+    
+    CDataReader reader(m_fd, 8192*10);
+    auto r = reader.getBlock(8192*10);
+    EQ(size_t(4), r.s_nItems);
+    
+    const void* pReadItem = skipItems(r.s_pData, 2); // s.b. item.
+    const RingItemHeader* pHeader = reinterpret_cast<const RingItemHeader*>(pReadItem);
+    EQ(size_t(200), size_t(pHeader->s_size));
+    EQ(TEST_DATA, pHeader->s_type);
+    EQ(sizeof(std::uint32_t), size_t(pHeader->s_unused));
+    EQ(0, memcmp(item1, pReadItem, 200));
+    
+    pReadItem = skipItems(pReadItem);
+    pHeader = reinterpret_cast<const RingItemHeader*>(pReadItem);
+    EQ(size_t(100), size_t(pHeader->s_size));
+    EQ(TEST_DATA, pHeader->s_type);
+    EQ(sizeof(std::uint32_t), size_t(pHeader->s_unused));
+    EQ(0, memcmp(item2, pReadItem, 100));
 }
