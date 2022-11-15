465e93cd2cfaa111a309494b9a78173cf2694114
Ron Fox <fox@nscl.msu.edu>
2022-11-15 10:17:09 -0500

    *   Added testouttests.cpp *   Make it in the testOutput parallel application. *   Ensure that a failing test will fail Make.

---
+++ base/testouttests.cpp
@@ -0,0 +1,54 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     FRIB
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  testouttests.cpp
+ *  @brief: Tests for the success of the testOutput parallel program.
+ */
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/Asserter.h>
+#include "Asserts.h"
+#include "TreeParameter.h"
+#include "TreeVariable.h"
+#include "AnalysisRingItems.h"
+
+
+
+
+class outputtest : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(outputtest);
+    CPPUNIT_TEST(test_1);
+    CPPUNIT_TEST_SUITE_END();
+    
+private:
+
+public:
+    void setUp() {
+        
+    }
+    void tearDown() {
+        
+    }
+protected:
+    void test_1();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(outputtest);
+
+void outputtest::test_1()
+{
+    ASSERT(false);
+}
