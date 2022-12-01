e64de8ab35992fa2b4950482a8a3b416671ba0ca
Ron Fox <fox@nscl.msu.edu>
2022-12-01 08:50:11 -0500

    Got setup/teardown to worki

--- base/worker1Tests.cpp
+++ base/worker1Tests.cpp
@@ -24,19 +24,31 @@
 #include <cppunit/Asserter.h>
 #include "Asserts.h"
 
+#include "AnalysisRingItems.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string>
+
+extern std::string filename;
+
+using namespace frib::analysis;
+
 class worker1test : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(worker1test);
     CPPUNIT_TEST(test_1);
     CPPUNIT_TEST_SUITE_END();
     
 private:
-
+    int m_fd;
 public:
     void setUp() {
-        
+        m_fd = open(filename.c_str(), O_RDONLY);
+        ASSERT(m_fd >= 0);
     }
     void tearDown() {
-        
+        close(m_fd);
     }
 protected:
     void test_1();
