77f47a3e13e539a89cf2bd5610751efa189d62d0
Ron Fox <fox@nscl.msu.edu>
2022-12-20 12:03:32 -0500

    In setup just suck the whole file into member data

--- base/parinworkertests.cpp
+++ base/parinworkertests.cpp
@@ -21,27 +21,45 @@
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/Asserter.h>
 #include "Asserts.h"
+#include "AnalysisRingItems.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-class aTestSuite : public CppUnit::TestFixture {
-    CPPUNIT_TEST_SUITE(aTestSuite);
-    CPPUNIT_TEST(test_1);
+using namespace frib::analysis;
+
+extern std::string workerFile;
+extern int         numberEvents;
+
+const size_t BUFFER_SIZE = 1024*1024*32;
+class parinworkertest : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(parinworkertest);
+    CPPUNIT_TEST(params_1);
     CPPUNIT_TEST_SUITE_END();
     
 private:
-
+    int m_fd;
+    std::uint8_t*   m_contents;
+    size_t         m_nBytes;
 public:
     void setUp() {
-        
+        m_contents = new std::uint8_t[BUFFER_SIZE];
+        m_fd = open(workerFile.c_str(), O_RDONLY);
+        ASSERT(m_fd >= 0);
+        m_nBytes = read(m_fd, m_contents, BUFFER_SIZE);
+        ASSERT(m_nBytes > 0);
     }
     void tearDown() {
-        
+        close(m_fd);
+        delete []m_contents;
     }
 protected:
-    void test_1();
+    void params_1();
 };
 
-CPPUNIT_TEST_SUITE_REGISTRATION(aTestSuite);
+CPPUNIT_TEST_SUITE_REGISTRATION(parinworkertest);
 
-void aTestSuite::test_1()
+void parinworkertest::params_1()
 {
 }
