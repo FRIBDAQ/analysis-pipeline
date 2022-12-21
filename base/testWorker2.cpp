34d514f72ef3409bed5a1677abe3bc3f15ee7d2f
Ron Fox <fox@nscl.msu.edu>
2022-12-21 10:36:09 -0500

    *   Put down a marker for a full integration test of the parameter to parameter parallel framework. *   Add to the Makefile so it gets buit and run in PARTESTS.

---
+++ base/testWorker2.cpp
@@ -0,0 +1,27 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     FRIB
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  testWorker2.cpp
+ *  @brief: Tests for parameters --> parameters worker(s).
+ */
+// Note this can be run with parallel workers.ss
+
+
+
+int main(int argc, char** argv) {
+    return 0;
+}
